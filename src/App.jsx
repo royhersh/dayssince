af73700746d76ca74d20aa5bbc600f592216eddf
Roy Hershkovitz <roy.hersh@gmail.com>
2019-03-15 21:03:10 +0200

    basic markup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+     <div className="app wrapper">
+      <div className="header">Days Since</div>
+
+      <div className="items-container">
+        <div className="item">
+          <div className="item--counter">34</div>
+          <div className="item--title">Car Wash</div>
+          <div className="item--edit-button">E</div>
+        </div>
+      </div>
+
+      <div className="items-container">
+        <div className="item">
+          <div className="item--counter">64</div>
+          <div className="item--title">Dentist</div>
+          <div className="item--edit-button">E</div>
+        </div>
+      </div>
+
+      <div className="add-button">+</div>
+     </div>
+    );
+  }
+}
+
+export default App;
