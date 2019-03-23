f78f9064e0b1ac072a241d2c2f9b30894a812f24
Roy Hershkovitz <roy.hersh@gmail.com>
2019-03-23 21:23:56 +0200

    add some css

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,82 @@ import './sass/main.scss';
 class App extends Component {
   render() {
     return (
-     <div className="app wrapper">
+     <div className="app-wrapper">
       <div className="header">Days Since</div>
 
       <div className="items-container">
         <div className="item">
-          <div className="item--counter">34</div>
-          <div className="item--title">Car Wash</div>
-          <div className="item--edit-button">E</div>
+          <div className="item__counter">34</div>
+          <div className="item__title">Car Wash</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
         </div>
 
         <div className="item">
-          <div className="item--counter">64</div>
-          <div className="item--title">Dentist</div>
-          <div className="item--edit-button">E</div>
+          <div className="item__counter">64</div>
+          <div className="item__title">Dentist</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
         </div>
+        
+        <div className="item">
+          <div className="item__counter">123</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
+        <div className="item">
+          <div className="item__counter">8664</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
+        <div className="item">
+          <div className="item__counter">34</div>
+          <div className="item__title">Car Wash</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
+        <div className="item">
+          <div className="item__counter">64</div>
+          <div className="item__title">Dentist</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+        
+        <div className="item">
+          <div className="item__counter">123</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+        
+        <div className="item">
+          <div className="item__counter">8664</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
+        <div className="item">
+          <div className="item__counter">34</div>
+          <div className="item__title">Car Wash</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
+        <div className="item">
+          <div className="item__counter">64</div>
+          <div className="item__title">Dentist</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+        
+        <div className="item">
+          <div className="item__counter">123</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+        
+        <div className="item">
+          <div className="item__counter">8664</div>
+          <div className="item__title">Something I didn't do for a long time</div>
+          <div className="item__edit-button"><i className="fas fa-edit"></i></div>
+        </div>
+
       </div>
 
       <div className="add-button">+</div>
