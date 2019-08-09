72851a1c4211a31011bc2c3af375d302ea2ab9b9
Roy Hershkovitz <roy.hersh@gmail.com>
2019-08-09 09:58:53 +0300

    testing initial data fetching flow

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -8,24 +8,37 @@ import { App } from '../App';
 
 require('jest-localstorage-mock');
 
+jest.mock('../utils/api');
+Enzyme.configure({ adapter: new Adapter() });
+
 function flushPromises() {
   return new Promise(resolve => setImmediate(resolve));
 }
 
-jest.mock('../utils/api');
-Enzyme.configure({ adapter: new Adapter() });
-
-const [populateItems, createNewItem] = new Array(2).fill(jest.fn());
-const props = {
-  populateItems,
-  createNewItem,
-  location: {
-    search: '?token=tokenString',
-  },
-};
-
 describe('Testing initial data fetch flow', () => {
-  it('merge items from localStorage to API when has token in URL', async () => {
+  let props;
+  let populateItems;
+  let createNewItem;
+
+  beforeEach(() => {
+    [populateItems, createNewItem] = new Array(2).fill(jest.fn());
+
+    props = {
+      populateItems,
+      createNewItem,
+      location: {
+        search: '',
+      },
+    };
+
+    localStorage.clear();
+  });
+
+  afterEach(() => {
+    // populateItems.mockClear();
+  });
+
+  it('merge items from localStorage to API when has token in URL and populate merged data', async () => {
     /**
      * If we have token in URL, then:
      * 1. send to API merge request with items in localStorage
@@ -33,16 +46,41 @@ describe('Testing initial data fetch flow', () => {
      * 3. set token in localStorage
      * 4. remove items in localStorage
      */
-    const dataInLocalStorage = 'some data';
+    const dataInLocalStorage = 'data in localStorage';
     localStorage.setItem('items', JSON.stringify(dataInLocalStorage));
     api.POST.mergeItems.mockImplementation(async () => 'merged data');
 
-    await shallow(<App {...props} />);
+    await shallow(
+      <App
+        {...props}
+        location={{
+          search: '?token=tokenString',
+        }}
+      />,
+    );
     await flushPromises();
 
     expect(api.POST.mergeItems.mock.calls[0][0]).toEqual(dataInLocalStorage);
     expect(populateItems.mock.calls[0][0]).toEqual('merged data');
     expect(localStorage.setItem).toHaveBeenLastCalledWith('token', 'tokenString');
     expect(localStorage.removeItem).toHaveBeenLastCalledWith('items');
+  });
+
+  it("populate data from API when there's token in localStorage", async () => {
+    localStorage.setItem('token', 'token exist in localStorage');
+    api.GET.items.mockImplementation(async () => 'data from API');
+
+    await shallow(<App {...props} />);
+    await flushPromises();
+    expect(populateItems.mock.calls[0][0]).toEqual('data from API');
+  });
+
+  it("populate data from localStorage when there's no token", async () => {
+    const dataInLocalStorage = 'data in localStorage';
+    localStorage.setItem('items', JSON.stringify(dataInLocalStorage));
+
+    await shallow(<App {...props} />);
+    await flushPromises();
+    expect(populateItems.mock.calls[0][0]).toEqual('data in localStorage');
   });
 });
