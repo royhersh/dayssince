820ae605b01d4be3da57298f0964385c7ee5ff62
Roy Hershkovitz <roy.hersh@gmail.com>
2019-09-15 22:47:18 +0300

    for heroku

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,17 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
+  console.log('NODE_ENV', process.env.NODE_ENV);
+  console.log('PLATFORM', process.env.PLATFORM);
+  let serverDomain;
+
+  switch (process.env.PLATFORM) {
+    case 'docker-compose':
+      serverDomain = 'server';
+      break;
+
+    default:
+      serverDomain = 'localhost';
+  }
   app.use(proxy(['/api', '/auth/google', '/dayssince/api'], { target: 'http://server:5000' }));
 };
