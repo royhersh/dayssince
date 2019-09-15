3d55f1621ca5823eb19b963de32158f5accb4042
Roy Hershkovitz <roy.hersh@gmail.com>
2019-09-15 23:16:27 +0300

    http to https on heroku

--- server/serverApp.js
+++ server/serverApp.js
@@ -23,6 +23,14 @@ if (process.env.NODE_ENV !== 'test') {
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`);
+    else next();
+  });
+}
+
 app.use(bodyParser.json());
 app.use(passport.initialize());
 
