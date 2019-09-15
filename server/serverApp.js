35d5cb1e336ec7db9017df27795a0a034b1c0179
Roy Hershkovitz <roy.hersh@gmail.com>
2019-09-15 22:47:18 +0300

    for heroku

--- server/serverApp.js
+++ server/serverApp.js
@@ -30,7 +30,7 @@ authRoutes(app);
 app.use('/dayssince/api', daysSinceRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('../client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
